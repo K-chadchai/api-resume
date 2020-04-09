8b9848455e761497cf75a398490ceb3e4d600201
Nikom <nikom@pongtewinlaos-MacBook-Pro.local>
2020-04-09 12:19:32 +0700

    Runing

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
+import { ValidationPipe, Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
+const log = new Logger();
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({ logger: false }),
+  );
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(3000, '0.0.0.0', (_, address) => log.log(`> Gateway is running ... ` + address));
 }
 bootstrap();
