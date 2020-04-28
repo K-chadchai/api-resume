4f771e012f98eb249534de432b56e395b2ddd52a
นิคม <snikom1723@gmail.com>
2020-04-28 14:15:29 +0700

    x

--- micros/src/pokemon/pokemon.module.ts
+++ micros/src/pokemon/pokemon.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PokemonService } from './pokemon.service';
 import { PokemonController } from './pokemon.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([PokemonModule])],
   providers: [PokemonService],
   controllers: [PokemonController],
 })
