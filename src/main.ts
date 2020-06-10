a34e9ef576c549b6aa2e68e4b6cfa4bdebd727fa
คม-นิคม <snikom1723@gmail.com>
2020-06-10 12:54:10 +0700

    allow

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: '*',
-    methods: 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
     allowedHeaders: '*',
+    methods: 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
   });
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new QueryFailedErrorFilter());
