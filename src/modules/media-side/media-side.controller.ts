25365fe43e6e4365ac008f95e1272f85a1208ba1
boonchoey <boonchoey@gmail.com>
2020-06-22 09:16:08 +0700

    update customcode

--- src/modules/media-side/media-side.controller.ts
+++ src/modules/media-side/media-side.controller.ts
@@ -3,38 +3,38 @@ import { Crud } from '@nestjsx/crud';
 import { MediaSideEntity } from 'src/entities/media_side.entity';
 import { MediaSideService } from './media-side.service';
 
-@Crud({
-    model: { type: MediaSideEntity },
-    params: { id: { field: 'id', type: 'uuid', primary: true } },
-})
+// @Crud({
+//     model: { type: MediaSideEntity },
+//     params: { id: { field: 'id', type: 'uuid', primary: true } },
+// })
 @Controller('media-side')
 export class MediaSideController {
     constructor(public service: MediaSideService) { }
 
-    // @Get('side')
-    // async getSides(@Query() query) {
-    //     return await this.service.getSides(query)
-    // }
+    @Get()
+    async get(@Query() query) {
+        return await this.service.get(query)
+    }
 
-    // @Get('side/:id')
-    // async getSide(@Param('id') id) {
-    //     return await this.service.getSide(id)
-    // }
+    @Get('/:id')
+    async getById(@Param('id') id) {
+        return await this.service.getById(id)
+    }
 
-    // @Put('side')
-    // async EditSide(@Body() query) {
-    //     return await this.service.EditSide(query)
-    // }
+    @Put()
+    async put(@Body() body) {
+        return await this.service.update(body)
+    }
 
-    // @Delete('side/:id')
-    // async DeleteSide(@Param('id') id) {
-    //     return await this.service.DeleteSide(id)
-    // }
+    @Delete('/:id')
+    async delete(@Param('id') id) {
+        return await this.service.delete(id)
+    }
 
-    // @Post('SaveSide')
-    // async SaveSide(@Body() query) {
-    //     return await this.service.SaveSide(query)
-    // }
+    @Post()
+    async post(@Body() body) {
+        return await this.service.post(body)
+    }
 
 
 }
--- src/modules/media-side/media-side.service.ts
+++ src/modules/media-side/media-side.service.ts
@@ -6,7 +6,7 @@ import { Like, QueryRunner } from 'typeorm';
 import { AppService } from 'src/app/app.service';
 
 interface IgetSides {
-    paging: number,
+    page_no: number,
     search: string
 }
 
@@ -30,138 +30,61 @@ export class MediaSideService extends TypeOrmCrudService<MediaSideEntity> {
         super(repo);
     }
 
-    //custom Api 
-
-    async getSides(props: IgetSides) {
-
-        const skip = (props.paging - 1) * 10 || 0
-        console.log("skip" + skip)
-
-        // Validate 
-
-        //process
-        return await this.appService.dbRunner(async (runner: QueryRunner) => {
-
-            const Sides = MediaSideEntity;
-            if (props.search) {
-                console.log("ใส่ค้นหา")
-                console.log(props.search)
-                const Side = await runner.manager.find(MediaSideEntity, {
-                    where: {
-                        side_name: Like(`%${props.search}%`)
-                    },
-                    order: {
-                        created_time: 'ASC'
-                    },
-                    skip: skip,
-                    take: 10
-                });
-                console.log(Side)
-                return { Side }
-            } else {
-                console.log("ไม่ใส่ค้นหา")
-                const Side = await runner.manager.find(MediaSideEntity, {
-                    order: {
-                        created_time: 'ASC'
-                    },
-                    skip: skip,
-                    take: 10
-                });
-                console.log(Side)
-                return { Side }
-            }
-            return { Sides };
+    // ค้นหาข้อมูล
+    async get(props: IgetSides) {
+        // console.log('props :>> ', props);
+        return await this.repo.find({
+            where: props.search ? {
+                side_name: Like(`%${props.search}%`)
+            } : '',
+            order: {
+                created_time: 'ASC'
+            },
+            skip: props.page_no > 0 ? ((props.page_no - 1) * 10) : 0,
+            take: 10
         });
     }
 
-    async getSide(id: string) {
+    // ค้นหาข้อมูล by id
+    async getById(id: string) {
+        return await this.repo.findOne({
+            where: [
+                { id: id }
+            ]
+        });
+    }
 
-        // Validate 
-        console.log(id)
-        if (!id) {
-            this.throwBadRequestException('กรุณาตรวจสอบ id')
+    // Update
+    async update(body) {
+        // Validate
+        if (!body.id) {
+            this.throwBadRequestException("ไม่พบข้อมูล, id");
         }
-        return await this.appService.dbRunner(async (runner: QueryRunner) => {
-
-            const Side = await runner.manager.find(MediaSideEntity, {
-                where: [
-                    { id: id }
-                ]
-            });
-            return { Side };
-        });
+        if (!await this.repo.findOne({ where: { id: body.id } })) {
+            this.throwBadRequestException(`ไม่พบข้อมูล, id=${body.id}`);
+        }
+        // Process
+        return await this.repo.save(body);
     }
 
-    async EditSide(query: IEditSide) {
-
-        console.log("id :" + query.id)
-        return await this.appService.dbRunner(async (runner: QueryRunner) => {
-            //
-            console.log("เข้าฟังชั่น อัพเดท", query)
-            const SideToUpdate: MediaSideEntity = await runner.manager.findOne(MediaSideEntity, {
-                where: [
-                    { id: query.id }
-                ]
-            })
-            console.log('SideToUpdate >>', SideToUpdate)
-            if (!SideToUpdate.id) {
-                throw new BadRequestException(
-                    `ไม่พบข้อมูล id :(${query.id}) ,กรุณาตรวจสอบ`,
-                );
-            }
-            //
-            SideToUpdate.side_name = query.side_name;
-            SideToUpdate.description = query.description;
-            SideToUpdate.last_edidor = query.last_edidor;
-            SideToUpdate.last_edited_time = query.last_edited_time;
-
-            console.log("ค่า side_name" + query.side_name)
-            const Sides = await runner.manager.save(MediaSideEntity, SideToUpdate);
-            // this.throwBadRequestException("555");
-            return Sides;
-        });
+    // Delete
+    async delete(id: string) {
+        // Validate
+        if (!id) {
+            this.throwBadRequestException("ไม่พบข้อมูล, id");
+        }
+        // Find data
+        const data = await this.repo.findOne({
+            where: { id: id }
+        })
+        if (!data) {
+            this.throwBadRequestException(`ไม่พบข้อมูล, id=${id}`);
+        }
+        // Process
+        return await this.repo.remove(data);
     }
 
-    async DeleteSide(id: string) {
-
-        console.log("Delete id :" + id)
-        return await this.appService.dbRunner(async (runner: QueryRunner) => {
-            //
-            const SideToUpdate: MediaSideEntity = await runner.manager.findOne(MediaSideEntity, {
-                where: [
-                    { id: id }
-                ]
-            })
-            if (!SideToUpdate.id) {
-                throw new BadRequestException(
-                    `ไม่พบข้อมูล id :(${id}) ,กรุณาตรวจสอบ`,
-                );
-            }
-            //
-            console.log("ข้อมูลค้นหา :" + SideToUpdate.id)
-            const Sides = await runner.manager.remove(MediaSideEntity, SideToUpdate);
-            // this.throwBadRequestException("555");
-            return Sides;
-
-        });
-    }
-
-    async SaveSide(query: MediaSideEntity) {
-
-        query.created_time = new Date();
-        query.last_edited_time = new Date();
-
-        return this.repo.save(query)
-
-        // console.log("id :" + query.id)
-        // return await this.appService.dbRunner(async (runner: QueryRunner) => {
-        //     //
-        //     //
-        //     const Sides = await runner.manager.save(MediaSideEntity, query);
-
-
-        //     // this.throwBadRequestException("555");
-        //     return Sides;
-        // });
+    async post(body: MediaSideEntity) {
+        return this.repo.save(body)
     }
 }
