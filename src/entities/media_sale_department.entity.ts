bcc36b1556ebad2c32e1f2731e0f2c2e3cc80645
boonchoey <boonchoey@gmail.com>
2020-07-08 08:43:20 +0700

    update interface and entity

--- src/entities/media_sale_department.entity.ts
+++ src/entities/media_sale_department.entity.ts
@@ -1,45 +1,44 @@
-import { IMediaSaleDepartmentEntity } from "src/interfaces/media_sale_department.interface"
-import { PrimaryGeneratedColumn, Column, Entity, Unique } from "typeorm";
+import { IMediaSaleDepartmentEntity } from 'src/interfaces/media_sale_department.interface';
+import { PrimaryGeneratedColumn, Column, Entity, Unique } from 'typeorm';
 
 const tname = 'media_sale_department';
 
 @Entity(tname)
 @Unique(`uc_${tname}_code`, ['code'])
 export class MediaSaleDepartmentEntity implements IMediaSaleDepartmentEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
-    @PrimaryGeneratedColumn('uuid')
-    id: string;
+  @Column({
+    length: 10,
+  })
+  code: string;
 
-    @Column({
-        length: 10
-    })
-    code: string;
+  @Column({
+    nullable: true,
+    length: 255,
+  })
+  description: string;
 
-    @Column({
-        nullable: true,
-        length: 255
-    })
-    descripion: string;
+  @Column({
+    nullable: true,
+    length: 15,
+  })
+  creator: string;
 
-    @Column({
-        nullable: true,
-        length: 15
-    })
-    creator: string;
+  @Column({
+    nullable: true,
+  })
+  created_time: Date;
 
-    @Column({
-        nullable: true
-    })
-    created_time: Date;
+  @Column({
+    nullable: true,
+    length: 15,
+  })
+  last_editor: string;
 
-    @Column({
-        nullable: true,
-        length: 15
-    })
-    last_editor: string;
-
-    @Column({
-        nullable: true
-    })
-    last_edited_time: Date;
+  @Column({
+    nullable: true,
+  })
+  last_edited_time: Date;
 }
--- src/interfaces/media_sale_department.interface.ts
+++ src/interfaces/media_sale_department.interface.ts
@@ -1,9 +1,9 @@
 export interface IMediaSaleDepartmentEntity {
-    id: string,
-    code: string,
-    descripion: string,
-    creator: string,
-    created_time: Date,
-    last_editor: string,
-    last_edited_time: Date
+  id: string;
+  code: string;
+  description: string;
+  creator: string;
+  created_time: Date;
+  last_editor: string;
+  last_edited_time: Date;
 }
