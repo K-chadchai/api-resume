ea078bb64471c0de97e80cb43750602f1502ce57
boonchoey <boonchoey@gmail.com>
2020-07-29 12:08:52 +0700

    update media object

--- src/modules/media-object/media-object.controller.ts
+++ src/modules/media-object/media-object.controller.ts
@@ -1,18 +1,32 @@
-import { Controller, Get, Query, Param, Put, Body, Delete, Post } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Query,
+  Param,
+  Put,
+  Body,
+  Delete,
+  Post,
+} from '@nestjs/common';
 import { MediaObjectService } from './media-object.service';
 import { Crud } from '@nestjsx/crud';
 import { MediaObjectEntity } from 'src/entities/media_object.entity';
 
 @Crud({
-    model: { type: MediaObjectEntity },
-    params: { id: { field: 'id', type: 'uuid', primary: true } },
+  model: { type: MediaObjectEntity },
+  params: { id: { field: 'id', type: 'uuid', primary: true } },
 })
 @Controller('media-object')
 export class MediaObjectController {
-    constructor(public service: MediaObjectService) { }
+  constructor(public service: MediaObjectService) {}
 
-    @Get('/paging')
-    async get(@Query() query) {
-        return await this.service.getPaging(query)
-    }
+  @Get('/paging')
+  async get(@Query() query) {
+    return await this.service.getPaging(query);
+  }
+
+  @Get('/byFolder_id/:id')
+  async getByFolderId(@Param('id') id) {
+    return await this.service.getByFolderId(id);
+  }
 }
--- src/modules/media-object/media-object.service.ts
+++ src/modules/media-object/media-object.service.ts
@@ -6,34 +6,44 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Like } from 'typeorm';
 
 interface IPostBulk {
-    bulk: MediaObjectEntity[]
+  bulk: MediaObjectEntity[];
 }
 
 interface IGetObjectService {
-    page_no: number,
-    search: string
+  page_no: number;
+  search: string;
 }
 
 @Injectable()
 export class MediaObjectService extends TypeOrmCrudService<MediaObjectEntity> {
-    constructor(
-        @InjectRepository(MediaObjectEntity) repo,
-        private readonly appService: AppService
-    ) {
-        super(repo);
-    }
+  constructor(
+    @InjectRepository(MediaObjectEntity) repo,
+    private readonly appService: AppService,
+  ) {
+    super(repo);
+  }
 
-    // ค้นหาข้อมูล
-    async getPaging(props: IGetObjectService) {
-        return await this.repo.find({
-            where: props.search ? {
-                object_name: Like(`%${props.search}%`)
-            } : '',
-            order: {
-                object_name: 'ASC'
-            },
-            skip: props.page_no > 0 ? ((props.page_no - 1) * 10) : 0,
-            take: 10
-        });
-    }
+  // ค้นหาข้อมูล
+  async getPaging(props: IGetObjectService) {
+    return await this.repo.find({
+      where: props.search
+        ? {
+            object_name: Like(`%${props.search}%`),
+          }
+        : '',
+      order: {
+        object_name: 'ASC',
+      },
+      skip: props.page_no > 0 ? (props.page_no - 1) * 10 : 0,
+      take: 10,
+    });
+  }
+
+  // ค้นหาข้อมูล ByFolderId
+  async getByFolderId(id: string) {
+    console.log('id', id);
+    return await this.repo.find({
+      where: [{ folder_id: id }],
+    });
+  }
 }
