993898c1771ebdd82a1ee0ac80934e8d61ae00f9
คม-นิคม <snikom1723@gmail.com>
2020-08-25 09:54:31 +0700

    KeysHeader

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,1 +1,6 @@
 export const DBAUTHOR = 'DBAUTHOR';
+
+export const KeysHeader = {
+  ApiValidateKey: 'api-validate-key',
+  ApiModuleId: 'api-module-id',
+};
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,6 +3,7 @@ import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './local-auth.guard';
 import { JwtAuthGuard } from './jwt-auth.guard';
 import { JWT_VALIDATE_KEY } from '@nikom.san/api-common';
+import { KeysHeader } from 'src/app/app.constants';
 
 @Controller('auth')
 export class AuthController {
@@ -15,7 +16,7 @@ export class AuthController {
   }
 
   @Post('jwt-validate')
-  async jwtValidate(@Headers('api-validate-key') jwtValidateKey, @Body('token') tokenPayload) {
+  async jwtValidate(@Headers(KeysHeader.ApiValidateKey) jwtValidateKey, @Body('token') tokenPayload) {
     if (jwtValidateKey !== JWT_VALIDATE_KEY) {
       throw new UnauthorizedException('Invalidate jwt-validate-key');
     }
@@ -37,7 +38,7 @@ export class AuthController {
 
   @UseGuards(JwtAuthGuard)
   @Get('user-roles')
-  async userRole(@Headers('api-module-id') moduleId, @Request() req) {
+  async userRole(@Headers(KeysHeader.ApiModuleId) moduleId, @Request() req) {
     return await this.authService.getUserRoles(moduleId, req.user);
   }
 }
