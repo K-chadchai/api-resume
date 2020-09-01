54c8fe5f4961a9692225792731927c14ab70c5af
คม-นิคม <snikom1723@gmail.com>
2020-09-01 22:08:09 +0700

    src/jwt

---
+++ src/jwt/jwt.strategy.ts
@@ -0,0 +1,38 @@
+/* eslint-disable @typescript-eslint/camelcase */
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable, NotFoundException, InternalServerErrorException } from '@nestjs/common';
+import { JwtConstant, TokenDto, routeApiAuth } from '@newsolution/api-authen';
+import axios from 'axios';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: JwtConstant.SECRET_KEY,
+    });
+  }
+
+  async validate(token: TokenDto) {
+    // return await this.authService.jwtValidate(token);
+    return await axios
+      .post(
+        routeApiAuth.getUrl(process.env.API_AUTHEN_HOST, routeApiAuth.jwtValidate),
+        { token },
+        {
+          headers: {
+            'api-validate-key': JwtConstant.VALIDATE_KEY,
+          },
+        },
+      )
+      .then((response) => {
+        if (response.data) return response.data;
+        throw new NotFoundException(`jwtValidate : Not found ,response.data`);
+      })
+      .catch((err) => {
+        throw new InternalServerErrorException(`jwtValidate : API Error, ${err}`);
+      });
+  }
+}
