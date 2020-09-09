13e1e97fc075d91380852f839926b55f719c712a
คม-นิคม <snikom1723@gmail.com>
2020-09-09 14:27:35 +0700

    routeApiAuth

--- libs/src/routes/_url.ts
+++ libs/src/routes/_url.ts
@@ -7,3 +7,15 @@ export class Api {
     return `${host}/${version}/${this.module}${route ? `/${route}` : ''}`;
   }
 }
+
+// export const routeApiAuth = {
+//   url: (host: string, route = '', query?: object) => url(host, 'v1', 'auth', route, query),
+//   login: 'login',
+//   loginToken: 'loginToken',
+//   logout: 'logout',
+//   jwtValidate: 'jwtValidate',
+//   killUser: 'killUser',
+//   payload: 'payload',
+//   userRoles: 'userRoles',
+//   moduleEnabled: 'moduleEnabled',
+// };
