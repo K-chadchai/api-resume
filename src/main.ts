1b96dbaf14a693cdac7ba880e64269453e944b96
คม-นิคม <snikom1723@gmail.com>
2020-09-14 13:44:08 +0700

    FastifyAdapter

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app/app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { JwtAuthGuard } from './jwt/jwt-auth.guard';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+
+const log = new Logger();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  // const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter({ logger: false }));
   // Enable JWT
   const reflector = app.get(Reflector);
   app.useGlobalGuards(new JwtAuthGuard(reflector));
@@ -15,6 +19,6 @@ async function bootstrap() {
     methods: 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
   });
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(4000);
+  await app.listen(4000, '0.0.0.0', (_, address) => log.log(`> ${address} ... ${process.env.NODE_ENV}`));
 }
 bootstrap();
