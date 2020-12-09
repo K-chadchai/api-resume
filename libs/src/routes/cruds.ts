99a45be79750ab140bcf4ac3c27170701889ce76
John <john@Johns-MacBook-Pro.local>
2020-12-09 07:24:54 +0700

    cruds

---
+++ libs/src/routes/cruds.ts
@@ -0,0 +1,12 @@
+// import { getCruds, getCrudPut, getCrudPost, getCrudPostBulk, getCrudDelete } from '@newsolution/api-common';
+
+// export const routeApiTableName = getCruds('table-name');
+
+// export const routeApiTableName = {
+//   ...getCrudPut('table-name'),
+//   ...getCrudPost('table-name'),
+//   ...getCrudPostBulk('table-name'),
+//   ...getCrudDelete('table-name'),
+// };
+
+export {};
